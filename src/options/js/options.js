7b64f91e08618aea285f7fec6fd8200209298e96
Nicolas Provencher <nick@onintime.com>
2021-01-26 12:22:42 -0500

    Report to SpamCop,Custom, or all + enable deleting/moving reported emails

--- src/options/js/options.js
+++ src/options/js/options.js
@@ -1,17 +1,45 @@
 // Locales setup
+$("#tab1").html(browser.i18n.getMessage("general.tab.1"));
+$("#tab2").html(browser.i18n.getMessage("general.tab.2"));
+$("#tab3").html(browser.i18n.getMessage("general.tab.3"));
+$("#tab4").html(browser.i18n.getMessage("general.tab.4"));
+$("#tab5").html(browser.i18n.getMessage("general.tab.5"));
+
 $("#menu1 #title").html(browser.i18n.getMessage("options.menu1.title"));
-$("#menu1 #description").html(browser.i18n.getMessage("options.menu1.description"));
+$("#menu1 #description").html(browser.i18n.getMessage("options.menu1.description") + "&emsp;&emsp;&emsp;");
+$("#menu1 #title2").html(browser.i18n.getMessage("options.menu1.title2"));
+$("#menu1 #description2").html(browser.i18n.getMessage("options.menu1.description2") + "&emsp;&emsp;&emsp;");
+$("#menu1 #leave").html(browser.i18n.getMessage("options.menu1.leave"));
+$("#menu1 #delete").html(browser.i18n.getMessage("options.menu1.delete"));
+$("#menu1 #move").html(browser.i18n.getMessage("options.menu1.move"));
 
 $("#menu2 #title").html(browser.i18n.getMessage("options.menu2.title"));
-$("#menu2 #description").html(browser.i18n.getMessage("options.menu2.description"));
-$("#menu2 #label1").html(browser.i18n.getMessage("options.menu2.label1"));
-$("#menu2 #serverURL").attr("placeholder", browser.i18n.getMessage("options.menu2.serverURL"));
-$("#menu2 #serverURLHelp").html(browser.i18n.getMessage("options.menu2.serverURLHelp"));
-$("#menu2 #label2").html(browser.i18n.getMessage("options.menu2.label2"));
-$("#menu2 #apiKey").attr("placeholder", browser.i18n.getMessage("options.menu2.apiKey"));
-$("#menu2 #apiKeyHelp").html(browser.i18n.getMessage("options.menu2.apiKeyHelp"));
-$("#menu2 #submit").html(browser.i18n.getMessage("options.menu2.submit"));
-$("#menu2 #reset").html(browser.i18n.getMessage("options.menu2.reset"));
+$("#menu2 #description").html(browser.i18n.getMessage("options.menu2.description") + "&emsp;&emsp;&emsp;");
+
+$("#menu3 #title").html(browser.i18n.getMessage("options.menu3.title"));
+$("#menu3 #description").html(browser.i18n.getMessage("options.menu3.description") + "&emsp;&emsp;&emsp;");
+$("#menu3 #label1").html(browser.i18n.getMessage("options.menu3.label1"));
+$("#menu3 #serverURL").attr("placeholder", browser.i18n.getMessage("options.menu3.serverURL"));
+$("#menu3 #serverURLHelp").html(browser.i18n.getMessage("options.menu3.serverURLHelp"));
+$("#menu3 #label2").html(browser.i18n.getMessage("options.menu3.label2"));
+$("#menu3 #apiKey").attr("placeholder", browser.i18n.getMessage("options.menu3.apiKey"));
+$("#menu3 #apiKeyHelp").html(browser.i18n.getMessage("options.menu3.apiKeyHelp"));
+$("#menu3 #submit").html(browser.i18n.getMessage("general.submit"));
+$("#menu3 #reset").html(browser.i18n.getMessage("general.reset"));
+
+$("#menu4 #title").html(browser.i18n.getMessage("options.menu4.title"));
+$("#menu4 #description").html(browser.i18n.getMessage("options.menu4.description") + "&emsp;&emsp;&emsp;");
+$("#menu4 #label1").html(browser.i18n.getMessage("options.menu4.label1"));
+$("#menu4 #spamcop").html(browser.i18n.getMessage("options.menu4.spamcop"));
+$("#menu4 #spamcopHelp").html(browser.i18n.getMessage("options.menu4.spamcopHelp"));
+$("#menu4 #submit").html(browser.i18n.getMessage("general.submit"));
+
+$("#menu5 #title").html(browser.i18n.getMessage("options.menu5.title"));
+$("#menu5 #description").html(browser.i18n.getMessage("options.menu5.description") + "&emsp;&emsp;&emsp;");
+$("#menu5 #label1").html(browser.i18n.getMessage("options.menu5.label1"));
+$("#menu5 #customEmail").html(browser.i18n.getMessage("options.menu5.customEmail"));
+$("#menu5 #customEmailHelp").html(browser.i18n.getMessage("options.menu5.customEmailHelp"));
+$("#menu5 #submit").html(browser.i18n.getMessage("general.submit"));
 
 // Main Interface
 $("li").on("click", function(){
@@ -19,24 +47,92 @@ $("li").on("click", function(){
   $("#" + this.id).addClass("ui-state-active");
 });
 
-$("input").on("click", function(){
+$("#menu1 input[name=mode]").on("click", function(){
+  var mode = $("#" + this.id)[0].id;
+  browser.storage.local.set({"mode":mode});
+});
+
+$("#menu1 input[name=action]").on("click", function(){
+  var action = $("#" + this.id)[0].id;
+  browser.storage.local.set({"action":action});
+});
+
+$("#menu2 input").on("click", function(){
   var whois = ($("#" + this.id)[0].id == "whois1")? "https://whois.com/whois/" : "https://who.is/whois/";
   browser.storage.local.set({"whois":whois});
 });
 
-$("#submit").on("click", function(){
+$("#menu3 #submit").on("click", function(){
   browser.storage.local.set({"server":$("#serverURL").val()})
   browser.storage.local.set({"api-key":$("#serverURL").val()})
 });
 
-$("#reset").on("click", function(){
+$("#menu3 #reset").on("click", function(){
   $("#serverURL").val("");
   $("#apiKey").val("");
   browser.storage.local.set({"server":"https://api.justreport.it/lookup/"})
   browser.storage.local.set({"api-key":"API-KEY"})
 });
 
+$("#menu4 #submit").on("click", function(){
+  browser.storage.local.set({"spamcop":$("#spamcopID").val()})
+});
+
+$("#menu5 #submit").on("click", function(){
+  var temp = $("#customEmail").val()
+  if (temp.split(",").length > 1){
+    var custom = [];
+    temp.split(",").forEach((element) => custom.push(element));
+    browser.storage.local.set({"custom":custom})
+  }
+  else
+    browser.storage.local.set({"custom":[temp]})
+});
+
 $(document).ready(function(){
+  browser.storage.local.get("mode").then((item) => {
+    if (item.mode == "registrar"){
+      $("#registrar").prop("checked", true);
+      $("#spamcop").prop("checked", false);
+      $("#custom").prop("checked", false);
+      $("#all").prop("checked", false);
+    }
+    else if (item.mode == "spamcop"){
+      $("#registrar").prop("checked", false);
+      $("#spamcop").prop("checked", true);
+      $("#custom").prop("checked", false);
+      $("#all").prop("checked", false);
+    }
+    else if (item.mode == "custom"){
+      $("#registrar").prop("checked", false);
+      $("#spamcop").prop("checked", false);
+      $("#custom").prop("checked", true);
+      $("#all").prop("checked", false);
+    }
+    else {
+      $("#registrar").prop("checked", false);
+      $("#spamcop").prop("checked", false);
+      $("#custom").prop("checked", false);
+      $("#all").prop("checked", true);
+    }
+  });
+  browser.storage.local.get("action").then((item) => {
+    if (item.action == "leave"){
+      $("#leave").prop("checked", true);
+      $("#delete").prop("checked", false);
+      $("#move").prop("checked", false);
+    }
+    else if (item.action == "delete"){
+      $("#leave").prop("checked", false);
+      $("#delete").prop("checked", true);
+      $("#move").prop("checked", false);
+    }
+    else {
+      $("#leave").prop("checked", false);
+      $("#delete").prop("checked", false);
+      $("#move").prop("checked", true);
+    }
+  });
   browser.storage.local.get("whois").then((item) => {
     if (item.whois == "https://whois.com/whois/") {
       $("#whois1").prop("checked", true);
@@ -55,5 +151,12 @@ $(document).ready(function(){
     if (item["api-key"] != "API-KEY")
       $("#apiKey").val(item["api-key"]);
   });
+  browser.storage.local.get("spamcop").then((item) => {
+    $("#spamcopID").val(item.spamcop);
+  });
+  browser.storage.local.get("custom").then((item) => {
+    var custom = "";
+    item.custom.forEach((element) => custom += element + ",");
+    $("#customEmail").val(custom.slice(0, -1));
+  });
 });
-
--- src/scripts/background.js
+++ src/scripts/background.js
@@ -2,29 +2,93 @@
 browser.storage.local.get("whois").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"whois":"https://whois.com/whois/"})}})
 browser.storage.local.get("server").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"server":"https://api.justreport.it/lookup/"})}})
 browser.storage.local.get("api-key").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"api-key":"API-KEY"})}})
+browser.storage.local.get("mode").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"mode":"registrar"})}})
+browser.storage.local.get("spamcop").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"spamcop":""})}})
+browser.storage.local.get("action").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"action":"leave"})}})
+browser.storage.local.get("custom").then((item) => {if (Object.entries(item).length==0){browser.storage.local.set({"custom":[]})}})
 
 browser.messageDisplayAction.onClicked.addListener((tab) =>{
   browser.messageDisplay.getDisplayedMessage(tab.id).then((message) => {
     browser.messages.getRaw(message.id).then((raw) => {
-      var spamDomain = extractSpamDomain(message.author);
-      getAbuseEmail(spamDomain).then((x) => x().then((y) => y().then((response) => {
-        if (response.status == "success"){
-          composeEmail(response.data.email, spamDomain, raw)
+      browser.storage.local.get("mode").then((configuration) => {
+        var spamDomain = extractSpamDomain(message.author);
+        var to = [];
+        if (configuration.mode == "registrar" || configuration.mode == "all"){
+          getAbuseEmail(spamDomain).then((x) => x().then((y) => y().then((response) => {
+            if (configuration.mode == "all"){
+              getSpamcopEmail().then((email) => {
+                getCustomEmail().then((custom) => {
+                  to.push.apply(to, custom);
+                  to.push(email);
+                  if (response.status == "success") {
+                    to.push(response.data.email);
+                    composeEmail(to, spamDomain, raw);
+                  }
+                  else
+                    composeEmail(to, spamDomain, raw).then(() => {
+                      createPopup(spamDomain).then((popup) => popup());
+                    });
+                })
+              });
+            }
+            else {
+              if (response.status == "success") {
+                to.push(response.data.email);
+                composeEmail(to, spamDomain, raw);
+              }
+              else
+                composeEmail(to, spamDomain, raw).then(() => {
+                  createPopup(spamDomain).then((popup) => popup());
+                });
+            }
+          })));
         }
-        else {
-          composeEmail("", spamDomain, raw).then(() => {
-            createPopup(spamDomain).then((popup) => popup());
+        else if (configuration.mode == "custom")
+          getCustomEmail().then((custom) => {
+            to.concat(custom);
+            composeEmail(to, spamDomain, raw);
           });
-        }
-      })));
+        else
+          getSpamcopEmail().then((email) => {
+            to.push(email);
+            composeEmail(to, spamDomain, raw);
+          });
+        performAction(message.id);
+      });
     });
   });
-})
+});
+
+async function performAction(messageId){
+  browser.storage.local.get("action").then((item) => {
+    if (item.action == "move"){
+      browser.messages.delete([messageId], false);
+    }
+    else if (item.action == "delete") {
+      browser.messages.delete([messageId], true);
+    }
+  });
+}
+
+function getSpamcopEmail(){
+  return browser.storage.local.get("spamcop").then((item) => {
+      return "submit." + item.spamcop + "@spam.spamcop.net";
+  });
+}
+
+function getCustomEmail(){
+  return browser.storage.local.get("custom").then((item) => {
+      return item.custom;
+  });
+}
 
 async function composeEmail(to, domain, raw){
-  await browser.compose.beginNew({to: to,
+  let file = new File([raw], "message.eml");
+  await browser.compose.beginNew({
+    to: to,
     subject: browser.i18n.getMessage("background.subject") + domain,
-    plainTextBody: getBody(domain, raw)
+    plainTextBody: getBody(domain),
+    attachments: [{file:file}]
   });
 }
 
@@ -44,9 +108,8 @@ async function setFocused(window){
   });
 }
 
-function getBody(spamDomain, rawSpam){
-  return browser.i18n.getMessage("background.body.1") + spamDomain + ".\n" +
-          browser.i18n.getMessage("background.body.2") + rawSpam
+function getBody(spamDomain){
+  return browser.i18n.getMessage("background.body");
 }
 
 function extractSpamDomain(author){
